7cd114e48c2af7e902b4db9765eecf61b053bab9
YD525 <525084464@qq.com>
2026-01-02 18:29:57 +0800

    UPDate

--- SSEAT_Lex/ModManagement/ModReader.cs
+++ SSEAT_Lex/ModManagement/ModReader.cs
@@ -4,26 +4,44 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PhoenixEngine.ConvertManager;
 
 namespace SSEAT_Lex.ModManagement
 {
     public class ModReader
     {
-        public bool IsMod(string Path)
-        { 
-        
-        }
-        public void TestSCanModPath(string Path)
+        public class ModItem
         {
-            foreach (var GetDir in Directory.GetDirectories(Path))
+            public string Name { get; set; }
+            public string Path { get; set; }
+
+            public ModItem(string Path)
             { 
-            
+               this.Path = Path;
+               this.Name = new FileInfo(Path).Name;
             }
         }
-
-        public void SetEnableMods(string Path)
-        { 
-        
+        public bool IsMod(string Path)
+        {
+            string[] Extensions = new string[] { "*.esp", "*.pex", "*.esm", "*.esl", "*.bsa" };
+            var Files = Extensions.SelectMany(ext => Directory.GetFiles(Path, ext, SearchOption.AllDirectories)).ToArray();
+            if (Files.Length > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public List<ModItem> TestSCanModPath(string Path)
+        {
+            List<ModItem> Mods = new List<ModItem>();
+            foreach (var GetDir in Directory.GetDirectories(Path))
+            {
+                if (IsMod(GetDir))
+                {
+                    Mods.Add(new ModItem(GetDir));
+                }
+            }
+            return Mods;
         }
     }
 }
--- SSEAT_Lex_Bridge/MainWindow.xaml.cs
+++ SSEAT_Lex_Bridge/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SSEAT_Lex.ModManagement;
 
 namespace SSEAT_Lex_Bridge
 {
@@ -23,6 +24,11 @@ namespace SSEAT_Lex_Bridge
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var GetMods = new ModReader().TestSCanModPath(@"C:\Users\52508\Desktop\1TestMod");
         }
     }
 }
